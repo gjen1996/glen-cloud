4eddfb221f90dca4cf177437c866cde450556192
gaiyc <gaiyc@chinaunicom.cn>
2019-06-18 17:10:36 +0800

    msg

--- config-client/src/main/java/com/example/configclient/controller/configClientController.java
+++ config-client/src/main/java/com/example/configclient/controller/configClientController.java
@@ -5,6 +5,7 @@ package com.example.configclient.controller;/**
  */
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @Description
  */
 @RestController
+@RefreshScope
 public class configClientController {
     @Value("${neo.hello}")
     private String hello;
